7c6b783b566751a776bf1339dfa6780a26adf755
Khalifa <bodakhalifa707@gmail.com>
2024-09-10 23:23:53 +0300

    parrallax mouse hover effect

---
+++ items.js
@@ -0,0 +1,43 @@
+
+export  const items = [
+    {
+        img: "./img/03hero.jpg",
+        parallaxSpeed: 0.065
+    },
+    {
+        img: "./img/07hero.jpg",
+        parallaxSpeed: 0.05
+    },
+    {
+        img: "./img/08hero.jpg",
+        parallaxSpeed: 0.1
+    },
+    {
+        img: "./img/heritage01.jpg",
+        parallaxSpeed: 0.1
+    },
+    {
+        img: "./img/heritage02.jpg",
+        parallaxSpeed: 0.07
+    },
+    {
+        img: "./img/heritage03.jpg",
+        parallaxSpeed: 0.085
+    },
+    {
+        img: "./img/heritage04.jpg",
+        parallaxSpeed: 0.06
+    },
+    {
+        img: "./img/about.jpg",
+        parallaxSpeed: 0.04
+    },
+    {
+        img: "./img/03hero.jpg",
+        parallaxSpeed: 0.01
+    },
+    {
+        img: "./img/07hero.jpg",
+        parallaxSpeed: 0.065
+    }
+];
